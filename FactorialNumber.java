1264b011686d3f873679dc2e7dce4dd8d07642c0
karthimathi2000@gmail.com <email>
2023-09-02 14:04:03 +0530

    first commit

---
+++ FactorialNumber.java
@@ -0,0 +1,16 @@
+package practice;
+
+public class FactorialNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	int factorial=1;
+	int fact=3;
+	for(int i=1;i<=fact;i++) {
+		factorial=factorial*i;
+	}
+		System.out.println(factorial);
+	}
+
+}
